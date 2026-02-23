9eed56bcb845e7883e4dc1e458c3c31e57a73822
je <limjueun06@gmail.com>
2026-02-23 18:19:15 +0900

    20260223학습기록

---
+++ ex01_Basic/ex02module.js
@@ -0,0 +1,9 @@
+// 모듈(module) : 독립된 기능을 가지고 있는 코드의 조각!
+
+// 내가 만든 모듈을 불러오는 작업!
+// require() : 모듈 사용을 위해 사용되는 기능!
+// => 모듈 불러오기!
+const {odd, even} = require('./ex02짝홀수.js')
+
+console.log(odd)
+console.log(even)
---
+++ ex01_Basic/ex02짝홀수.js
@@ -0,0 +1,5 @@
+const odd = "홀수입니다"
+const even = "짝수입니다"
+
+// 모듈로 위의 내용 추출(= 내보내기) 하기!
+module.exports = {odd, even}
